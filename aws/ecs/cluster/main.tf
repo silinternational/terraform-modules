598015039abd70dfaffff334c6f196249ad7badc
Phillip Shipley <phillip.shipley@gmail.com>
2017-04-13 14:50:49 -0400

    update ecs cluster

--- aws/ecs/cluster/main.tf
+++ aws/ecs/cluster/main.tf
@@ -20,13 +20,16 @@ data "aws_ami" "ecs_ami"{
 /*
  * Create ECS IAM Instance Role and Policy
  */
+resource "random_id" "code" {
+  byte_length = 4
+}
 resource "aws_iam_role" "ecsInstanceRole" {
-  name = "ecsInstanceRole"
+  name = "ecsInstanceRole-${random_id.code.hex}"
   assume_role_policy = "${var.ecsInstanceRoleAssumeRolePolicy}"
 }
 
 resource "aws_iam_role_policy" "ecsInstanceRolePolicy" {
-  name = "ecsInstanceRolePolicy"
+  name = "ecsInstanceRolePolicy-${random_id.code.hex}"
   role = "${aws_iam_role.ecsInstanceRole.id}"
   policy = "${var.ecsInstancerolePolicy}"
 }
@@ -35,17 +38,17 @@ resource "aws_iam_role_policy" "ecsInstanceRolePolicy" {
  * Create ECS IAM Service Role and Policy
  */
 resource "aws_iam_role" "ecsServiceRole" {
-  name = "ecsServiceRole"
+  name = "ecsServiceRole-${random_id.code.hex}"
   assume_role_policy = "${var.ecsServiceRoleAssumeRolePolicy}"
 }
 
 resource "aws_iam_role_policy" "ecsServiceRolePolicy" {
-  name = "ecsServiceRolePolicy"
+  name = "ecsServiceRolePolicy-${random_id.code.hex}"
   role = "${aws_iam_role.ecsServiceRole.id}"
   policy = "${var.ecsServiceRolePolicy}"
 }
 
 resource "aws_iam_instance_profile" "ecsInstanceProfile" {
-  name = "ecsInstanceProfile"
+  name = "ecsInstanceProfile-${random_id.code.hex}"
   roles = ["${aws_iam_role.ecsInstanceRole.name}"]
 }
