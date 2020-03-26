ea60b47b9a64bf4ac3f44e35d1b365bcca05fcaf
Dale Newby <dale_newby@sil.org>
2020-03-26 16:22:02 -0400

    aws/ecr upgrade to Terraform 0.12

--- aws/ecr/main.tf
+++ aws/ecr/main.tf
@@ -2,20 +2,21 @@
  * Create ECR repository
  */
 resource "aws_ecr_repository" "repo" {
-  name = "${var.repo_name}"
+  name = var.repo_name
 }
 
 data "template_file" "repo_policy" {
-  template = "${file("${path.module}/ecr-policy.json")}"
+  template = file("${path.module}/ecr-policy.json")
 
-  vars {
-    ecsInstanceRole_arn = "${var.ecsInstanceRole_arn}"
-    ecsServiceRole_arn  = "${var.ecsServiceRole_arn}"
-    cd_user_arn         = "${var.cd_user_arn}"
+  vars = {
+    ecsInstanceRole_arn = var.ecsInstanceRole_arn
+    ecsServiceRole_arn  = var.ecsServiceRole_arn
+    cd_user_arn         = var.cd_user_arn
   }
 }
 
 resource "aws_ecr_repository_policy" "policy" {
-  repository = "${aws_ecr_repository.repo.name}"
-  policy     = "${data.template_file.repo_policy.rendered}"
+  repository = aws_ecr_repository.repo.name
+  policy     = data.template_file.repo_policy.rendered
 }
+
