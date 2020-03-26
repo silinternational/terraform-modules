fcb23fcc9bc184639228950bcfbd297e0e3eef43
Dale Newby <dale_newby@sil.org>
2020-03-26 16:24:45 -0400

    aws/ecs/service-no-alb-with-volume upgrade to Terraform 0.12

--- aws/ecs/service-no-alb-with-volume/outputs.tf
+++ aws/ecs/service-no-alb-with-volume/outputs.tf
@@ -2,32 +2,33 @@
  * Task definition outputs
  */
 output "task_def_arn" {
-  value = "${aws_ecs_task_definition.td.arn}"
+  value = aws_ecs_task_definition.td.arn
 }
 
 output "task_def_family" {
-  value = "${aws_ecs_task_definition.td.family}"
+  value = aws_ecs_task_definition.td.family
 }
 
 output "task_def_revision" {
-  value = "${aws_ecs_task_definition.td.revision}"
+  value = aws_ecs_task_definition.td.revision
 }
 
 /*
  * Service outputs
  */
 output "service_id" {
-  value = "${aws_ecs_service.service.id}"
+  value = aws_ecs_service.service.id
 }
 
 output "service_name" {
-  value = "${aws_ecs_service.service.name}"
+  value = aws_ecs_service.service.name
 }
 
 output "service_cluster" {
-  value = "${aws_ecs_service.service.cluster}"
+  value = aws_ecs_service.service.cluster
 }
 
 output "service_desired_count" {
-  value = "${aws_ecs_service.service.desired_count}"
+  value = aws_ecs_service.service.desired_count
 }
+
