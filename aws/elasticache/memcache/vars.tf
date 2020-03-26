9d3e8b0635ff43888952212d4b8df130ff6f68a3
Dale Newby <dale_newby@sil.org>
2020-03-26 16:30:29 -0400

    aws/elasticache/memcache upgrade to Terraform 0.12

--- aws/elasticache/memcache/vars.tf
+++ aws/elasticache/memcache/vars.tf
@@ -2,57 +2,58 @@
  * Required Variables
  */
 variable "cluster_id" {
-  type = "string"
+  type = string
 }
 
 variable "security_group_ids" {
-  type = "list"
+  type = list(string)
 }
 
 variable "subnet_group_name" {
-  type = "string"
+  type = string
 }
 
 variable "subnet_ids" {
-  type = "list"
+  type = list(string)
 }
 
 variable "availability_zones" {
-  type = "list"
+  type = list(string)
 }
 
 variable "app_name" {
-  type = "string"
+  type = string
 }
 
 variable "app_env" {
-  type = "string"
+  type = string
 }
 
 /*
  * Optional Variables
  */
 variable "node_type" {
-  type    = "string"
+  type    = string
   default = "cache.t2.micro"
 }
 
 variable "port" {
-  type    = "string"
+  type    = string
   default = "11211"
 }
 
 variable "num_cache_nodes" {
-  type    = "string"
+  type    = string
   default = 2
 }
 
 variable "parameter_group_name" {
-  type    = "string"
+  type    = string
   default = "default.memcached1.4"
 }
 
 variable "az_mode" {
-  type    = "string"
+  type    = string
   default = "cross-az"
 }
+
