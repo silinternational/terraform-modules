73ca30dba32b53d5cd01991a71dd15025b487686
Dale Newby <dale_newby@sil.org>
2020-03-26 16:17:36 -0400

    aws/alb upgrade to Terraform 0.12

--- aws/alb/main.tf
+++ aws/alb/main.tf
@@ -2,17 +2,17 @@
  * Create application load balancer
  */
 resource "aws_alb" "alb" {
-  name               = "${coalesce(var.alb_name, "alb-${var.app_name}-${var.app_env}")}"
-  internal           = "${var.internal}"
-  security_groups    = ["${var.security_groups}"]
-  subnets            = ["${var.subnets}"]
-  load_balancer_type = "${var.load_balancer_type}"
-  idle_timeout       = "${var.idle_timeout}"
+  name               = coalesce(var.alb_name, "alb-${var.app_name}-${var.app_env}")
+  internal           = var.internal
+  security_groups    = var.security_groups
+  subnets            = var.subnets
+  load_balancer_type = var.load_balancer_type
+  idle_timeout       = var.idle_timeout
 
-  tags {
-    Name     = "${coalesce(var.alb_name, "alb-${var.app_name}-${var.app_env}")}"
-    app_name = "${var.app_name}"
-    app_env  = "${var.app_env}"
+  tags = {
+    Name     = coalesce(var.alb_name, "alb-${var.app_name}-${var.app_env}")
+    app_name = var.app_name
+    app_env  = var.app_env
   }
 }
 
@@ -20,20 +20,20 @@ resource "aws_alb" "alb" {
  * Create target group for ALB
  */
 resource "aws_alb_target_group" "default" {
-  name     = "${coalesce(var.tg_name, "tg-${var.app_name}-${var.app_env}")}"
-  port     = "${var.port}"
-  protocol = "${var.protocol}"
-  vpc_id   = "${var.vpc_id}"
+  name     = coalesce(var.tg_name, "tg-${var.app_name}-${var.app_env}")
+  port     = var.port
+  protocol = var.protocol
+  vpc_id   = var.vpc_id
 
   health_check {
-    interval            = "${var.health_check_interval}"
-    path                = "${var.health_check_path}"
-    port                = "${var.health_check_port}"
-    protocol            = "${var.health_check_protocol}"
-    timeout             = "${var.health_check_timeout}"
-    healthy_threshold   = "${var.healthy_threshold}"
-    unhealthy_threshold = "${var.unhealthy_threshold}"
-    matcher             = "${var.health_check_status_codes}"
+    interval            = var.health_check_interval
+    path                = var.health_check_path
+    port                = var.health_check_port
+    protocol            = var.health_check_protocol
+    timeout             = var.health_check_timeout
+    healthy_threshold   = var.healthy_threshold
+    unhealthy_threshold = var.unhealthy_threshold
+    matcher             = var.health_check_status_codes
   }
 }
 
@@ -41,14 +41,15 @@ resource "aws_alb_target_group" "default" {
  * Create listeners to connect ALB to target group
  */
 resource "aws_alb_listener" "https" {
-  load_balancer_arn = "${aws_alb.alb.arn}"
+  load_balancer_arn = aws_alb.alb.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "${var.ssl_policy}"
-  certificate_arn   = "${var.certificate_arn}"
+  ssl_policy        = var.ssl_policy
+  certificate_arn   = var.certificate_arn
 
   default_action {
-    target_group_arn = "${aws_alb_target_group.default.arn}"
+    target_group_arn = aws_alb_target_group.default.arn
     type             = "forward"
   }
 }
+
