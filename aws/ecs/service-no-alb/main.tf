7ccd55ccac37041e33b1503d914e6afc3dbda6fb
Dale Newby <dale_newby@sil.org>
2020-03-26 16:25:59 -0400

    aws/ecs/service-no-alb upgrade to Terraform 0.12

--- aws/ecs/service-no-alb/main.tf
+++ aws/ecs/service-no-alb/main.tf
@@ -2,8 +2,8 @@
  * Get task definition data
  */
 data "aws_ecs_task_definition" "td" {
-  task_definition = "${aws_ecs_task_definition.td.family}"
-  depends_on      = ["aws_ecs_task_definition.td"]
+  task_definition = aws_ecs_task_definition.td.family
+  depends_on      = [aws_ecs_task_definition.td]
 }
 
 /*
@@ -11,20 +11,20 @@ data "aws_ecs_task_definition" "td" {
  */
 resource "aws_ecs_task_definition" "td" {
   family                = "${var.service_name}-${var.service_env}"
-  container_definitions = "${var.container_def_json}"
-  task_role_arn         = "${var.task_role_arn}"
-  network_mode          = "${var.network_mode}"
+  container_definitions = var.container_def_json
+  task_role_arn         = var.task_role_arn
+  network_mode          = var.network_mode
 }
 
 /*
  * Create ECS Service
  */
 resource "aws_ecs_service" "service" {
-  name                               = "${var.service_name}"
-  cluster                            = "${var.cluster_id}"
-  desired_count                      = "${var.desired_count}"
-  deployment_maximum_percent         = "${var.deployment_maximum_percent}"
-  deployment_minimum_healthy_percent = "${var.deployment_minimum_healthy_percent}"
+  name                               = var.service_name
+  cluster                            = var.cluster_id
+  desired_count                      = var.desired_count
+  deployment_maximum_percent         = var.deployment_maximum_percent
+  deployment_minimum_healthy_percent = var.deployment_minimum_healthy_percent
 
   ordered_placement_strategy {
     type  = "spread"
@@ -32,5 +32,9 @@ resource "aws_ecs_service" "service" {
   }
 
   # Track the latest ACTIVE revision
-  task_definition = "${aws_ecs_task_definition.td.family}:${max("${aws_ecs_task_definition.td.revision}", "${data.aws_ecs_task_definition.td.revision}")}"
+  task_definition = "${aws_ecs_task_definition.td.family}:${max(
+    aws_ecs_task_definition.td.revision,
+    data.aws_ecs_task_definition.td.revision,
+  )}"
 }
+
