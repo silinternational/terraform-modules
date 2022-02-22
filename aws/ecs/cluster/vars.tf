f094dbde31068b79cafd5dfc89ffb3b8b71e4039
Schparky <3172830+Schparky@users.noreply.github.com>
2022-02-22 14:47:45 -0700

    use jsonencode to ensure correct json syntax

--- aws/ecs/cluster/vars.tf
+++ aws/ecs/cluster/vars.tf
@@ -15,98 +15,90 @@ variable "app_env" {
 variable "ecsInstanceRoleAssumeRolePolicy" {
   type = string
 
-  default = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
+  default = jsonencode(
     {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-
+      Version = "2008-10-17"
+      Statement = [
+        {
+          Sid    = ""
+          Effect = "Allow"
+          Principal = {
+            Service = "ec2.amazonaws.com"
+          }
+          Action = "sts:AssumeRole"
+        },
+      ]
+  })
 }
 
 variable "ecsInstancerolePolicy" {
   type = string
 
-  default = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  default = jsonencode(
     {
-      "Effect": "Allow",
-      "Action": [
-        "ecs:CreateCluster",
-        "ecs:DeregisterContainerInstance",
-        "ecs:DiscoverPollEndpoint",
-        "ecs:Poll",
-        "ecs:RegisterContainerInstance",
-        "ecs:StartTelemetrySession",
-        "ecs:Submit*",
-        "ecr:GetAuthorizationToken",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = [
+            "ecs:CreateCluster",
+            "ecs:DeregisterContainerInstance",
+            "ecs:DiscoverPollEndpoint",
+            "ecs:Poll",
+            "ecs:RegisterContainerInstance",
+            "ecs:StartTelemetrySession",
+            "ecs:Submit*",
+            "ecr:GetAuthorizationToken",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents",
+          ]
+          Resource = "*"
+        }
+      ]
+  })
 }
 
 variable "ecsServiceRoleAssumeRolePolicy" {
   type = string
 
-  default = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
+  default = jsonencode(
     {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-
+      Version = "2008-10-17"
+      Statement = [
+        {
+          Sid    = ""
+          Effect = "Allow"
+          Principal = {
+            Service = "ecs.amazonaws.com"
+          }
+          Action = "sts:AssumeRole"
+        },
+      ]
+  })
 }
 
 variable "ecsServiceRolePolicy" {
-  default = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  default = jsonencode(
     {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:AuthorizeSecurityGroupIngress",
-        "ec2:Describe*",
-        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-        "elasticloadbalancing:DeregisterTargets",
-        "elasticloadbalancing:Describe*",
-        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-        "elasticloadbalancing:RegisterTargets"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = [
+            "ec2:AuthorizeSecurityGroupIngress",
+            "ec2:Describe*",
+            "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+            "elasticloadbalancing:DeregisterTargets",
+            "elasticloadbalancing:Describe*",
+            "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+            "elasticloadbalancing:RegisterTargets"
+          ]
+          Resource = "*"
+        },
+      ]
+  })
 }
 
