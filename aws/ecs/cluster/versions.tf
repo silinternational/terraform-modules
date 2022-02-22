4e307c4b8fbe48aa9070c5d8ca443e1f4bab51d7
Schparky <3172830+Schparky@users.noreply.github.com>
2022-02-22 16:25:57 -0700

    change <= to < in required_providers

--- aws/ecs/cluster/versions.tf
+++ aws/ecs/cluster/versions.tf
@@ -5,11 +5,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0.0, <= 5.0.0"
+      version = ">= 2.0.0, < 5.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 2.1.0, <= 4.0.0"
+      version = ">= 2.1.0, < 4.0.0"
     }
   }
 }
