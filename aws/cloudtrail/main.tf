8367b385c593d1878fdc0ac362d7970c17153fc5
Schparky <3172830+Schparky@users.noreply.github.com>
2022-02-22 14:48:50 -0700

    BC: use aws_s3_bucket_policy to allow use with aws provider version 4

--- aws/cloudtrail/main.tf
+++ aws/cloudtrail/main.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "cloudtrail" {
   bucket        = var.s3_bucket_name
   force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  bucket = aws_s3_bucket.cloudtrail.id
 
   policy = jsonencode(
     {
