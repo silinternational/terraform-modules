95dfbe82efb826294310982c65999c2d12c96b0d
Dale Newby <dale_newby@sil.org>
2023-06-15 16:49:24 -0400

    Update formatting

--- test/asg.tf
+++ test/asg.tf
@@ -3,7 +3,7 @@ module "asg-test" {
   app_name                = "test_name1"
   app_env                 = "test_env1"
   ami_id                  = "ami-1234567890"
-  aws_instance            = {instance_type="t3.nano", volume_size="8", instance_count="1"}
+  aws_instance            = { instance_type = "t3.nano", volume_size = "8", instance_count = "1" }
   private_subnet_ids      = ["1234", "5678"]
   default_sg_id           = "sg-asdf"
   ecs_instance_profile_id = "asdf"
@@ -11,14 +11,14 @@ module "asg-test" {
 }
 
 module "asg-ebs-test" {
-  source              = "../aws/asg-ebs"
+  source                  = "../aws/asg-ebs"
   app_name                = "test_name2"
   app_env                 = "test_env2"
   ami_id                  = "ami-1234567890"
-  aws_instance            = {instance_type="t3.nano", volume_size="8", instance_count="1"}
+  aws_instance            = { instance_type = "t3.nano", volume_size = "8", instance_count = "1" }
   aws_region              = "us-west-2"
-  aws_access_key          = "ALKJDFIEO"
-  aws_secret_key          = "asdfljkasdflkjasdflkjasdflkjasdflkjasdf"
+  aws_access_key          = "AKIA1234567890123456"
+  aws_secret_key          = "1234567890123456789012345678901234567890"
   private_subnet_ids      = ["1234", "5678"]
   default_sg_id           = "sg-asdf"
   ecs_instance_profile_id = "asdf"
