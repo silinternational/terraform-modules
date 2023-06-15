f47cda2a43a4da6da581fa2dc6956643406578ea
Dale Newby <dale_newby@sil.org>
2023-06-15 16:37:04 -0400

    Add tests for asg and asg-ebs

---
+++ test/asg.tf
@@ -0,0 +1,29 @@
+module "asg-test" {
+  source                  = "../aws/asg"
+  app_name                = "test_name1"
+  app_env                 = "test_env1"
+  ami_id                  = "ami-1234567890"
+  aws_instance            = {instance_type="t3.nano", volume_size="8", instance_count="1"}
+  private_subnet_ids      = ["1234", "5678"]
+  default_sg_id           = "sg-asdf"
+  ecs_instance_profile_id = "asdf"
+  ecs_cluster_name        = "mycluster1"
+}
+
+module "asg-ebs-test" {
+  source              = "../aws/asg-ebs"
+  app_name                = "test_name2"
+  app_env                 = "test_env2"
+  ami_id                  = "ami-1234567890"
+  aws_instance            = {instance_type="t3.nano", volume_size="8", instance_count="1"}
+  aws_region              = "us-west-2"
+  aws_access_key          = "ALKJDFIEO"
+  aws_secret_key          = "asdfljkasdflkjasdflkjasdflkjasdflkjasdf"
+  private_subnet_ids      = ["1234", "5678"]
+  default_sg_id           = "sg-asdf"
+  ecs_instance_profile_id = "asdf"
+  ecs_cluster_name        = "mycluster1"
+  ebs_device              = "/dev/sdh"
+  ebs_mount_point         = "/foo/bah"
+  ebs_vol_id              = "vol-123456789"
+}
