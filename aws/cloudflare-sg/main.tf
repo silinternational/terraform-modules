76e5cf3159313e233cfbc8fe30cc8dd958193264
briskt <3172830+briskt@users.noreply.github.com>
2024-04-15 16:14:52 -0600

    add ipv6 support to cloudflare-sg

--- aws/cloudflare-sg/main.tf
+++ aws/cloudflare-sg/main.tf
@@ -15,14 +15,5 @@ resource "aws_security_group_rule" "cloudflare_ipv4" {
   protocol          = "tcp"
   security_group_id = aws_security_group.cloudflare_https.id
   cidr_blocks       = module.cf_ips.ipv4_cidrs
+  ipv6_cidr_blocks  = module.cf_ips.ipv6_cidrs
 }
-
-// IPv6 not supported by security groups yet
-/*resource "aws_security_group_rule" "cloudflare_ipv6" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  security_group_id = "${aws_security_group.cloudflare_https.id}"
-  cidr_blocks = ["${module.cf_ips.ipv6_cidrs}"]
-}*/
